cc1a0e74634ed9e064db522716402674527c1ec6
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-02-06 10:11:47 +0530

    Added GSAP ScrollTrigger animation for video clipping on scroll
    Integrated GSAP's ScrollTrigger plugin to animate the video frame's clipping and border-radius properties dynamically based on scroll. This enhances the visual experience by creating a smooth and interactive scroll effect.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
 import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger)
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
     const [hasClicked, setHasClicked] = useState(false);
@@ -25,6 +27,12 @@ const Hero = () => {
         setHasClicked(true);
         setCurrentIndex(upcomingVideoIndex);
     }
+
+    useEffect(() => {
+        if(loadedVideos === totalVideos - 1) {
+            setIsLoading(false);
+        }
+    }, [loadedVideos]);
 
     useGSAP(() => {
         if(hasClicked) {
@@ -49,11 +57,46 @@ const Hero = () => {
         }
     }, {dependencies: [currentIndex], revertOnUpdate: true});
 
-    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+    // Scroll Animation
+    useGSAP(() => {
+        gsap.set("#video-frame", {
+            clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)",
+            borderRadius: "0 0 40% 10%"
+        });
 
+        gsap.from("#video-frame", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: "0 0 0 0",
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: "#video-frame",
+                start: "center center",
+                bottom: "bottom center",
+                scrub: true,
+            }
+        })
+    })
+
+
+    const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
         <div className="relative h-dvh w-screen overflow-x-hidden">
+            {isLoading && (
+                <div className="flex-center absolute z-100 h-dvh w-screen overflow-hidden bg-violet-50">
+                    <div className="three-body">
+                        <div className="three-body__dot">
+
+                        </div>
+                        <div className="three-body__dot">
+
+                        </div>
+                        <div className="three-body__dot">
+
+                        </div>
+                    </div>
+                </div>
+            )}
             <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
                 <div className="">
                     <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
