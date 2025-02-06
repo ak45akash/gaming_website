23733fce06f09c39e49f50b7756cba23fb95a4ba
Akashdeep Kancha <ak45.akashdeep@gmail.com>
2025-02-06 08:35:44 +0530

    Added customized fonts and assets file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ['zentery', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'circular-web': ['circular-web', 'sans-serif'],
+        'robert-medium': ['robert-medium', 'sans-serif'],
+        'robert-regular': ['robert-regular', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
